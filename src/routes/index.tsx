1883ca85e31d5c3486139ed177821afdb422e3c5
Joao Victor Rodrigues Delgado <jvrd1515@gmail.com>
2024-12-03 19:06:43 +0000

    add home
    Co-authored-by: André Augusto <AndreAugustoDev@users.noreply.github.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,33 @@
-import { Button } from "@/components/ui/button"
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { Link, createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
-
-
+export const Route = createFileRoute("/")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  const buttons = ["Clientes", "Estoque", "Dívidas", "Editar", "Cadastrar"]
+	const buttons = [
+		{ name: "Clientes", url: "/clients" },
+		{ name: "Estoque", url: "/stock" },
+		{ name: "Dívidas", url: "/debts" },
+	];
 
-  return (
-    <>
-      <div className="flex flex-col h-screen w-screen bg-zinc-700 justify-center items-center">
-        <h1 className="m-8 text-zinc-100 uppercase text-shadow text-shadow-x-md text-shadow-y-md text-shadow-blur-8 text-shadow-zinc-50">Controle de estoque</h1>
-        <div className="flex flex-col gap-2">
-          {buttons.map(button => (
-            <Button key={button} className="w-48 h-16">{button}</Button>
-          ))}
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="flex flex-col h-screen w-screen bg-zinc-700 justify-center items-center">
+				<h1 className="m-8 text-zinc-100 uppercase text-shadow text-shadow-x-md text-shadow-y-md text-shadow-blur-1 text-shadow-zinc-700">
+					Controle de estoque
+				</h1>
+				<div className="flex flex-col gap-2">
+					{buttons.map((button) => (
+						<Link to={button.url}>
+							<Button key={button.name} className="w-48 h-16">
+								{button.name}
+							</Button>
+						</Link>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
--- src/routes/stock/index.lazy.tsx
+++ src/routes/stock/index.lazy.tsx
@@ -1,9 +1,17 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createLazyFileRoute('/stock/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/stock/"!</div>
+  return(
+    <>
+      <div className="">
+        <Button>Teste</Button>
+        <table>cu</table>
+      </div>
+    </>
+  )
 }
